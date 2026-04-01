7370f65da03029df5d586feac9a91cbf490c8a22
Brotha Darkness <abrinson808@gmail.com>
2026-03-31 19:15:22 -0600

    Initial Commit

---
+++ PandorasBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PandorasBox
+//
+//  Created by Alex Brinson on 3/31/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PandorasBox/PandorasBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  PandorasBoxApp.swift
+//  PandorasBox
+//
+//  Created by Alex Brinson on 3/31/26.
+//
+
+import SwiftUI
+
+@main
+struct PandorasBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
